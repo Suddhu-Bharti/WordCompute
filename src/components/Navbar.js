efca451a5c9fa8e2b57a59861050c35cacb21f27
Suddhu Kumar <suddhubharti@gmail.com>
2023-05-07 09:44:38 +0530

    changes in navbar third time

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-a active" aria-current="page" href="https://github.com/Suddhu-Bharti/WordCompute/settings/pages">Home</a>
+            <a className="nav-a active" aria-current="page" href="https://wwww.google.com/">Home</a>
           </li>
           {/* <li className="nav-item">
             <a className="nav-a" href="/about">{props.aboutText}</a>
