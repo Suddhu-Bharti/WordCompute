434419d95eb1cecdae49ecd17e3057d2dca5c3a7
Suddhu Kumar <suddhubharti@gmail.com>
2023-05-07 10:22:41 +0530

    changes in navbar fifth time

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-a active" aria-current="page" href="#">Home</a>
+            <a className="nav-a active" aria-current="page" href="/">Home</a>
           </li>
           {/* <li className="nav-item">
             <a className="nav-a" href="/about">{props.aboutText}</a>
